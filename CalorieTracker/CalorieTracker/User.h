4c8c3c64718d76dbc2cb058db352bf8f5e53bafc
AsukaNakagawa <nina-jamie.elle14@live.jp>
2016-07-26 21:16:37 -0700

    added User and Day

---
+++ CalorieTracker/CalorieTracker/User.h
@@ -0,0 +1,27 @@
+//
+//  User.h
+//  CalorieTracker
+//
+//  Created by Asuka Nakagawa on 2016-07-26.
+//  Copyright © 2016 Reena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Realm/Realm.h>
+#import "Day.h"
+
+@interface User : RLMObject
+
+@property (nonatomic) NSString *name;
+@property (assign) int age;
+
+@property (nonatomic) NSString *gender;
+
+@property (assign) int weight;
+@property (assign) int height;
+
+@property (nonatomic) NSString *exerciseLevel;
+
+@property RLMArray<Day *><Day> *daysArray;
+
+@end
