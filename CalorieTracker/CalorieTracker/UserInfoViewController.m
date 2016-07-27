64a45a1b2ebd675120618f2e2423adb374f08c9f
AsukaNakagawa <nina-jamie.elle14@live.jp>
2016-07-27 11:42:51 -0700

    more updates

--- CalorieTracker/CalorieTracker/UserInfoViewController.m
+++ CalorieTracker/CalorieTracker/UserInfoViewController.m
@@ -8,7 +8,11 @@
 
 #import "UserInfoViewController.h"
 
-@interface UserInfoViewController ()
+@interface UserInfoViewController () {
+    NSArray *pickerExerciseLevel;
+}
+
+@property RLMResults<User *> *userArray;
 
 @end
 
@@ -16,13 +20,118 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    NSLog(@"config is %@", [[RLMRealm defaultRealm] configuration]);
+    self.userArray = [User allObjects];
+    
+    pickerExerciseLevel = @[@"Sedentary", @"Lightly active", @"Moderately active", @"Very active", @"Extra active"];
+
+    self.levelPickerView.delegate = self;
+    self.levelPickerView.dataSource = self;
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - PickerView Delegates
+// the number of columns of data
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
 }
+
+// the number of rows data
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return [pickerExerciseLevel count];
+}
+
+// The data to return for the row and component (column) that's being passed in
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return pickerExerciseLevel[row];
+}
+
+//-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+//    NSString *level = [pickerExerciseLevel objectAtIndex:row];
+//    
+//}
+
+#pragma mark - Calculate BMR
+- (IBAction)calculatePressed:(UIButton *)sender {
+    
+    User *user = [[User alloc] init];
+    user.name = self.nameTextField.text;
+    
+    int height = [self.heightTextField.text intValue];
+    user.height = height;
+
+    int weight = [self.weightTextField.text intValue];
+    user.weight = weight;
+    
+    int age = [self.ageTextField.text intValue];
+    user.age = age;
+    
+    user.gender = self.genderTextField.text;
+    
+    NSString *selectedLevel = [pickerExerciseLevel objectAtIndex:[self.levelPickerView selectedRowInComponent:0]];
+    NSLog(@"%@", selectedLevel);
+    
+    user.exerciseLevel = selectedLevel;
+    double multiplier = 0;
+    
+    if ([selectedLevel isEqualToString:@"Sedentary"]) {
+        multiplier = 1.2;
+        
+    } else if ([selectedLevel isEqualToString:@"Lightly active"]) {
+        multiplier = 1.375;
+        
+    } else if ([selectedLevel isEqualToString:@"Moderately active"]) {
+        multiplier = 1.55;
+        
+    } else if ([selectedLevel isEqualToString:@"Very active"]) {
+        multiplier = 1.725;
+        
+    } else {
+        multiplier = 1.9;
+    }
+    NSLog(@"multiplier--%.3f", multiplier);
+    
+    double preBMR = (10 * weight) + 6.25 * height - (5 * age);
+    
+    if ([user.gender isEqual:@"m"]) {
+        
+        double BMR = (preBMR + 5) * multiplier;
+        self.resultBMILabel.text = [NSString stringWithFormat:@"%.2f", BMR];
+        NSLog(@"BMR---%.2f", BMR);
+        
+    // if women
+    } else if ([user.gender isEqual:@"f"]) {
+        
+        double BMR = (preBMR - 161) * multiplier;
+        self.resultBMILabel.text = [NSString stringWithFormat:@"%.2f", BMR];
+        NSLog(@"BMR---%.2f", BMR);
+
+    } else {
+        NSLog(@"Nothing Happen");
+    }
+    RLMRealm *realm = [RLMRealm defaultRealm];
+    [realm transactionWithBlock:^{
+        [realm addObject:user];
+    }];
+    
+    self.userArray = [User allObjects];
+    
+}
+
+#pragma mark - TextField Delegates
+
+-(void)textFieldDidBeginEditing:(UITextField *)textField {
+}
+
+-(void)textFieldDidEndEditing:(UITextField *)textField {
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
+
 
 /*
 #pragma mark - Navigation
@@ -33,5 +142,7 @@
     // Pass the selected object to the new view controller.
 }
 */
-
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
 @end
